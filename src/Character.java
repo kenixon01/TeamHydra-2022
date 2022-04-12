f24746e517a79aa333afe4033fa9737d6f1fb717
Brian.Smithers <brianwsmithers@gmail.com>
2022-04-11 21:15:02 -0400

    Can now load all characters.
    Only issue right now is that the characters do not get their items yet. Not sure how to implement this at the moment.

--- src/Character.java
+++ src/Character.java
@@ -1,16 +1,21 @@
 import java.util.LinkedList;
 
+/**
+ * Author: Brian Smithers
+ */
+
 public class Character {
 
-    private final int id;
+    private final String id;
     private final String name;
     private final LinkedList<Item> playerItemInventory;
     private final String description;
     private int hitPoints;
     private double dodgeChance;
+    private double criticalHitChance;
     private int damage;
 
-    public Character(int id, String name, LinkedList<Item> playerItemInventory,
+    public Character(String id, String name, LinkedList<Item> playerItemInventory,
                      String description, int hitPoints, double dodgeChance, int damage) {
         this.id = id;
         this.name = name;
@@ -21,31 +26,110 @@ public class Character {
         this.damage = damage;
     }
 
+    // Create new Character object with choice 1 - 4.
+    public static Character loadCharacterData(int number) {
+        String characterFilePath;
+        String startingItemFilePath;
+        String characterDescriptionFilePath;
+        CharacterReader cr = null;
+        switch (number) {
+            case 1:
+                characterFilePath = "src/CharacterTextFiles/Character_01/CHAR_01.txt";
+                startingItemFilePath = "src/CharacterTextFiles/Character_01/" +
+                        "FlintLockPistolAndDagger.txt";
+                characterDescriptionFilePath = "src/CharacterTextFiles/Character_01/" +
+                        "CHAR_01_Description.txt";
+                cr = new CharacterReader(characterFilePath, startingItemFilePath,
+                        characterDescriptionFilePath);
+                cr.read();
+                break;
+            case 2:
+                characterFilePath = "src/CharacterTextFiles/Character_02/CHAR_02.txt";
+                startingItemFilePath = "src/CharacterTextFiles/Character_02/" +
+                        "DaggerOfDragons.txt";
+                characterDescriptionFilePath = "src/CharacterTextFiles/" +
+                        "Character_02/CHAR_02_Description.txt";
+                cr = new CharacterReader(characterFilePath, startingItemFilePath,
+                        characterDescriptionFilePath);
+                cr.read();
+                break;
+            case 3:
+                characterFilePath = "src/CharacterTextFiles/Character_03/CHAR_03.txt";
+                startingItemFilePath = "src/CharacterTextFiles/Character_03/" +
+                        "ShadowBow.txt";
+                characterDescriptionFilePath = "src/CharacterTextFiles/Character_03/" +
+                        "CHAR_03_Description.txt";
+                cr = new CharacterReader(characterFilePath, startingItemFilePath,
+                        characterDescriptionFilePath);
+                cr.read();
+                break;
+            case 4:
+                characterFilePath = "src/CharacterTextFiles/Character_04/CHAR_04.txt";
+                startingItemFilePath = "";
+                characterDescriptionFilePath = "src/CharacterTextFiles/Character_04/" +
+                        "CHAR_04_Description.txt";
+                cr = new CharacterReader(characterFilePath, startingItemFilePath,
+                        characterDescriptionFilePath);
+                cr.read();
+                break;
+            default: // TODO make default
+        }
+        assert cr != null;
+        return cr.getCharacter();
+    }
+
     //TODO Start Game Feature
 
     //TODO Save Game Feature
-
-    // TODO Basement Key Feature
 
     // TODO Character select
 
     // TODO Help Command
 
-    // TODO Puzzle Interact
-
     // TODO Inventory Command
 
     // TODO N, S, E, W Command for changing rooms
 
-    // TODO Equip Item
 
-    // TODO Regenerate Health
+    @Override
+    public String toString() {
+        return "Character{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", playerItemInventory=" + playerItemInventory +
+                ", description='" + description + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", dodgeChance=" + dodgeChance +
+                ", criticalHitChance=" + criticalHitChance +
+                ", damage=" + damage +
+                '}';
+    }
 
-    // TODO Unequip Item
+    public String getDescription() {
+        return description;
+    }
+}
 
-    // TODO Use Item
+class CharacterItem {
+    private String itemId;
+    private String itemName;
+    private String itemEffectiveness;
+    private String description;
+    private String location;
 
-    // TODO Pick Up Item
+    public CharacterItem(String itemId, String itemName, String itemEffectiveness, String description,
+                String location) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemEffectiveness = itemEffectiveness;
+        this.description = description;
+        this.location = location;
+    }
+}
 
-    // TODO Item Description
+class Tester {
+    public static void main(String[] args) {
+        Character testChar = Character.loadCharacterData(4);
+        System.out.println(testChar);
+    }
 }
