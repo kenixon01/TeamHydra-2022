d7365016788da4447ebedf255c6ff8a22a8e1c32
Jayson Dasher <58889274+jaysondasher@users.noreply.github.com>
2022-04-20 21:09:44 -0400

    Revert "trim before commiting to main"
    This reverts commit 1e9eb6bf80c805dc6c7214e0d40039f4b16a09b1.

---
+++ src/MonsterReader.java
@@ -0,0 +1,119 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * The MonsterReader class defines a monster's attributes and assigns values to those attributes
+ * by reading multiple text files. All monsters are constants, and instances of this class
+ * cannot alter their initial states.  All monsters have the following attributes:
+ * <blockquote>
+ *     <ul>
+ *         <li>ID - Monster's unique identifier</li>
+ *         <li>ROOM ID - Unique identification of the room where the monster lies</li>
+ *         <li>ITEM ID - Unique identification of the item that the monster drops upon death</li>
+ *         <li>HP - Monster's initial health point</li>
+ *         <li>DAMAGE - Monster's attack damage</li>
+ *         <li>NAME - Monster's name</li>
+ *         <li>DESCRIPTION - Information about the monster</li>
+ *     </ul>
+ * </blockquote>
+ *
+ * @since 1.0
+ * @author Khamilah E. Nixon
+ * @version 1.0
+ */
+public final class MonsterReader {
+
+    private static BufferedReader idFile, roomIDFile, itemIDFile, hpFile, damageFile, nameFile, descriptionFile;
+
+    private final int ID, ROOM_ID, ITEM_ID, HP, DAMAGE;
+
+    private final String NAME, DESCRIPTION;
+
+    static {
+        try {
+            idFile = new BufferedReader(new FileReader("src/MonsterTextFiles/ID.txt"));
+            roomIDFile = new BufferedReader(new FileReader("src/MonsterTextFiles/RoomID.txt"));
+            itemIDFile = new BufferedReader(new FileReader("src/MonsterTextFiles/ItemID.txt"));
+            hpFile = new BufferedReader(new FileReader("src/MonsterTextFiles/HP.txt"));
+            damageFile = new BufferedReader(new FileReader("src/MonsterTextFiles/Damage.txt"));
+            nameFile = new BufferedReader(new FileReader("src/MonsterTextFiles/Name.txt"));
+            descriptionFile = new BufferedReader(new FileReader("src/MonsterTextFiles/Description.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Assigns monster values based on values written in {@link #idFile}, {@link #idFile},
+     *  {@link #roomIDFile}, {@link #itemIDFile}, {@link #hpFile}, {@link #damageFile},
+     *  {@link #nameFile}, and {@link #descriptionFile}
+     */
+    public MonsterReader() {
+        ID = Integer.parseInt(readAttribute(idFile));
+        ROOM_ID = Integer.parseInt(readAttribute(roomIDFile));
+        ITEM_ID = Integer.parseInt(readAttribute(itemIDFile));
+        HP = Integer.parseInt(readAttribute(hpFile));
+        DAMAGE = Integer.parseInt(readAttribute(damageFile));
+        NAME = readAttribute(nameFile);
+        DESCRIPTION = readAttribute(descriptionFile);
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getROOM_ID() {
+        return ROOM_ID;
+    }
+
+    public int getITEM_ID() {
+        return ITEM_ID;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getDAMAGE() {
+        return DAMAGE;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public String getDESCRIPTION() {
+        return DESCRIPTION;
+    }
+
+    /**
+     * If the stream is not empty, the method will continue to read and store
+     * lines of text into a {@code StringBuilder} until the next read line
+     * is equivalent to the given String delimiter "----"
+     * @param file - The text file inside a {@code BufferedReader} stream
+     * @return block of text between the delimiters in the file
+     * @since 1.0
+     */
+    private String readAttribute(BufferedReader file) {
+        StringBuilder attribute = new StringBuilder();
+
+        try {
+            String next = file.readLine();
+
+            while (file.ready() && !next.equals("----")) {
+                attribute.append(next);
+                next = file.readLine();
+
+                //Add a newline to the end of the current line if the next line is not the delimiter
+                if (next.length() > 4) {
+                    attribute.append("\n");
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return attribute.toString();
+    }
+}
---
+++ src/TeamHydraFirstClass.java
@@ -0,0 +1,12 @@
+public class TeamHydraFirstClass {
+	public void briansMethod() {
+		System.out.println("Hello!");
+	}
+	public int add(int i, int j)
+	{
+		return i+j;
+	}
+	public void jayson(){
+		System.out.println("Hope this works.");
+	}
+}
