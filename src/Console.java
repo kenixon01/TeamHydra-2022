38405b3e70e43842ab430fd0c438b7701d8355dd
Brian.Smithers <brianwsmithers@gmail.com>
2022-04-21 22:34:36 -0400

    Added Validation and Switch Case

--- src/Console.java
+++ src/Console.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Console {
+    private String item = "";
     private String input = "";
 
     public String getInput() {
@@ -11,5 +12,25 @@ public class Console {
         input = "";
         Scanner scanner = new Scanner(System.in);
         input = scanner.nextLine().toLowerCase();
+    }
+
+    // This is used to validate input that is done to an object
+    public String inputValidator() {
+        String[] inputValidator = input.split("\\s+");
+        item = "";
+        if (inputValidator[0].equalsIgnoreCase("pickup")
+                || inputValidator[0].equalsIgnoreCase("inspect")
+                || inputValidator[0].equalsIgnoreCase("drop")
+                || inputValidator[0].equalsIgnoreCase("use")
+                || inputValidator[0].equalsIgnoreCase("equip")
+                || inputValidator[0].equalsIgnoreCase("unequip")) {
+            input = inputValidator[0];
+            // recombine string to pass for method
+            for (int i = 1; i < inputValidator.length; i++) {
+                item += inputValidator[i] + " ";
+            }
+            return input;
+        }
+        return input;
     }
 }
