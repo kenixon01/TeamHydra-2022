bb0cfbd7888486c50887b6c432d2064aab798523
Brian.Smithers <brianwsmithers@gmail.com>
2022-04-21 22:02:56 -0400

    Created Skeleton for Console MVC

---
+++ src/ConsoleController.java
@@ -0,0 +1,16 @@
+public class ConsoleController {
+
+    private Console console;
+    private ConsoleView consoleView;
+
+    public ConsoleController(Console model, ConsoleView view) {
+        this.console = model;
+        this.consoleView = view;
+    }
+
+    public void enterCommand() {
+        while (!console.getInput().equalsIgnoreCase("exit")) {
+            
+        }
+    }
+}
