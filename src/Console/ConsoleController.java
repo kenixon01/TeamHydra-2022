683cf8c2b69c62c33cddc503904e681859443ede
Khamilah Nixon <knixon4@ggc.edu>
2022-04-24 12:41:43 -0400

    Added all controllers, models, and view to console controller

--- src/Console/ConsoleController.java
+++ src/Console/ConsoleController.java
@@ -1,25 +1,61 @@
 package Console;
 
-import Character.CharacterController;
-import Battle.BattleController;
-import Item.ItemController;
-import Monster.MonsterController;
-import Puzzle.PuzzleController;
-import Room.RoomController;
+import Battle.*;
+import Character.Character;
+import Item.*;
+import Room.*;
+import Character.*;
+import Monster.*;
+import Puzzle.*;
 
 public class ConsoleController {
     private BattleController battleController;
+    private Battle battle;
+    private BattleView battleView;
     private CharacterController characterController;
+    private Character character;
+    private CharacterView characterView;
+
     private ItemController itemController;
+    private Item item;
+    private ItemView itemView;
+
     private MonsterController monsterController;
+    private Monster monster;
+    private MonsterView monsterView;
+
     private PuzzleController puzzleController;
+    private Puzzle puzzle;
+    private PuzzleView puzzleView;
+
     private RoomController roomController;
+    private Room room;
+    private RoomView roomView;
+
     private Console console;
     private ConsoleView consoleView;
 
-    public ConsoleController(Console model, ConsoleView view) {
-        this.console = model;
-        this.consoleView = view;
+    public ConsoleController(BattleController battleController, Battle battle, BattleView battleView, CharacterController characterController, Character character, CharacterView characterView, ItemController itemController, Item item, ItemView itemView, MonsterController monsterController, Monster monster, MonsterView monsterView, PuzzleController puzzleController, Puzzle puzzle, PuzzleView puzzleView, RoomController roomController, Room room, RoomView roomView, Console console, ConsoleView consoleView) {
+        this.battleController = battleController;
+        this.battle = battle;
+        this.battleView = battleView;
+        this.characterController = characterController;
+        this.character = character;
+        this.characterView = characterView;
+        this.itemController = itemController;
+        this.item = item;
+        this.itemView = itemView;
+        this.monsterController = monsterController;
+        this.monster = monster;
+        this.monsterView = monsterView;
+        this.puzzleController = puzzleController;
+        this.puzzle = puzzle;
+        this.puzzleView = puzzleView;
+        this.roomController = roomController;
+        this.room = room;
+        this.roomView = roomView;
+        this.console = console;
+        this.consoleView = consoleView;
     }
 
     /**
@@ -105,10 +141,12 @@ public class ConsoleController {
      * @author Khamilah Nixon
      */
     public void characterSelect() {
+        characterController.chooseCharacter();
         //call the CharacterView characterSelect
         String userOption = console.menuInputValidator(
                 new String[]{"1","2","3","4"}
         );
+        System.out.println("character selected");
         //placeholder for character selection functionality
     }
 
@@ -119,10 +157,23 @@ public class ConsoleController {
     public void enterCommand() {
         while (!console.getInput().equalsIgnoreCase("exit")) {
             console.enterCommand();
-
             switch (console.inputValidator()) {
-
-
+                case "inspect" -> System.out.println("inspect");
+                case "drop" -> System.out.println("drop");
+                case "use" -> System.out.println("use");
+                case "equip" -> System.out.println("equip");
+                case "unequip" -> System.out.println("unequip");
+                case "north" -> System.out.println("north");
+                case "east" -> System.out.println("east");
+                case "south" -> System.out.println("south");
+                case "west" -> System.out.println("west");
+                case "attack" -> System.out.println("attack");
+                case "inventory" -> System.out.println("inventory");
+                case "save" -> System.out.println("save");
+                case "resume" -> System.out.println("resume");
+                case "block" -> System.out.println("block");
+                case "dodge" -> System.out.println("dodge");
+                case "hint" -> System.out.println("hint");
                 default -> invalidCommand();
             }
         }
--- src/Entry/Entry.java
+++ src/Entry/Entry.java
@@ -1,11 +1,18 @@
 package Entry;
 
+
 import Console.Console;
 import Console.ConsoleController;
 import Console.ConsoleView;
+import Battle.*;
+import Character.Character;
+import Item.*;
+import Room.*;
+import Character.*;
+import Monster.*;
+import Puzzle.*;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 /**
  * Author: Brian Smithers
@@ -14,9 +21,39 @@ public class Entry {
 
     //Brian Smithers and Khamilah Nixon
     public static void main(String[] args) {
+        Character character = new Character();
+        CharacterView characterView = new CharacterView();
+        CharacterController characterController;
+
+        Item item = new Item();
+        ItemView itemView = new ItemView();
+        ItemController itemController = new ItemController();
+
+        Monster monster = new Monster();
+        MonsterView monsterView = new MonsterView();
+        MonsterController monsterController = new MonsterController(monster, monsterView);
+
+        Puzzle puzzle = new Puzzle();
+        PuzzleView puzzleView = new PuzzleView();
+        PuzzleController puzzleController = new PuzzleController();
+
+        Room room = new Room();
+        RoomView roomView = new RoomView();
+        RoomController roomController = new RoomController(room, roomView);
+
+        Battle battle = new Battle(character,monster);
+        BattleView battleView = new BattleView();
+        BattleController battleController = new BattleController(battle,battleView);
+
         Console console = new Console();
         ConsoleView consoleView = new ConsoleView();
-        ConsoleController consoleController = new ConsoleController(console, consoleView);
+        ConsoleController consoleController = new ConsoleController(
+                battleController,battle,battleView,
+                characterController, character, characterView,
+                itemController,item,itemView,
+                monsterController, monster,monsterView,
+                puzzleController, puzzle, puzzleView,
+                roomController, room, roomView, console, consoleView);
 
 
         try {
