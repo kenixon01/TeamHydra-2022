1c0c99e8cf774619acb0581c8b3da25d1b62e7a5
Jayson Dasher <58889274+jaysondasher@users.noreply.github.com>
2022-05-01 16:18:40 -0400

    started equip/unequip integration

--- src/Item/ItemController.java
+++ src/Item/ItemController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 /**
- *  Author: Jayson Dasher
+ * Author: Jayson Dasher
  */
 public class ItemController implements Serializable {
     // Room Number, Item for the Map
@@ -21,24 +21,24 @@ public class ItemController implements Serializable {
     }
 
     // EQUIP ITEM
-    public void equipItem(Character character){
+    public void equipItem(Character character) {
         //print inventory
-        view.printinventory(model.get("0"));
+        view.printinventory(character.getInventoryController().getItemInventory());
         //get input
         Scanner reader = new Scanner(System.in);
         String input = reader.nextLine();
-        Item currentItem = model.get("0").get(Integer.parseInt(input)-1);
+        Item currentItem = character.getInventoryController().getItemInventory().get(Integer.parseInt(input) - 1);
         //if item is weapon, check if there is an equipped item and reduce damage modifier accordingly, set isEquipped to false
-            //use inputted item damage value to alter character damage value and set isEquipped to true
-        if(currentItem.get_itemType().equalsIgnoreCase("weapon")){
+        //use inputted item damage value to alter character damage value and set isEquipped to true
+        if (currentItem.get_itemType().equalsIgnoreCase("weapon")) {
             //find if another weapon is equipped. unequip it and deduct damage value from character
-            for (int i = 0; i < model.get("0").size(); i++) {
-                if ((model.get("0").get(i).get_itemType().equalsIgnoreCase("weapon")
-                        && model.get("0").get(i).getEquipped())){
+            for (int i = 0; i < character.getInventoryController().getItemInventory().size(); i++) {
+                if ((character.getInventoryController().getItemInventory().get(i).get_itemType().equalsIgnoreCase("weapon")
+                        && character.getInventoryController().getItemInventory().get(i).getEquipped())) {
                     //set boolean for original equipped item to false (unequipping it)
-                    model.get("0").get(i).setEquipped(false);
+                    character.getInventoryController().getItemInventory().get(i).setEquipped(false);
                     //reduce character damage by weapons damage modifier
-                    character.setDamage(character.getDamage() - model.get("0").get(i).get_damageValue());
+                    character.setDamage(character.getDamage() - character.getInventoryController().getItemInventory().get(i).get_damageValue());
                     //set input selection eqipped value to true
                     currentItem.setEquipped(true);
                     //add weapon damage modifier to character damage
@@ -47,7 +47,7 @@ public class ItemController implements Serializable {
             }
         }
         //if item selected is wearable
-        else if(currentItem.get_itemType().equalsIgnoreCase("wearable")){
+        else if (currentItem.get_itemType().equalsIgnoreCase("wearable")) {
             //set equipped to true
             currentItem.setEquipped(true);
             // set player max health value to current value plus modifier
@@ -61,43 +61,41 @@ public class ItemController implements Serializable {
         }
     }
 
-    public void unequipItem(Character character){
+    public void unequipItem(Character character) {
         int equippedCount = 0;
         //check if any items in inventory are currently equipped
-        for (int i = 0; i < model.get("0").size(); i++) {
-            if (model.get("0").get(i).getEquipped() == true) {
+        for (int i = 0; i < character.getInventoryController().getItemInventory().size(); i++) {
+            if (character.getInventoryController().getItemInventory().get(i).getEquipped()) {
                 equippedCount += 1;
-                view.printNumName(model.get("0").get(i));
+                view.printNumName(character.getInventoryController().getItemInventory().get(i));
             }
         }
-            //if not, print out message
-            if (equippedCount == 0) {
-                view.noneEquipped();
+        //if not, print out message
+        if (equippedCount == 0) {
+            view.noneEquipped();
+        } else {
+            //get input
+            Scanner reader = new Scanner(System.in);
+            String input = reader.nextLine();
+            Item currentItem = character.getInventoryController().getItemInventory().get(Integer.parseInt(input));
+            //if input is equipped item, mark item unequipped and alter character values accordingly.
+            if (currentItem.getEquipped() == true) {
+                currentItem.setEquipped(false);
+                character.setDamage(character.getDamage() - currentItem.get_damageValue());
+                character.setHp(character.getHp() + currentItem.get_healValue());
             }
-            else {
-                //get input
-                Scanner reader = new Scanner(System.in);
-                String input = reader.nextLine();
-                Item currentItem = model.get("0").get(Integer.parseInt(input));
-                //if input is equipped item, mark item unequipped and alter character values accordingly.
-                if (currentItem.getEquipped() == true){
-                    currentItem.setEquipped(false);
-                    character.setDamage(character.getDamage() - currentItem.get_damageValue());
-                    character.setHp(character.getHp() + currentItem.get_healValue());
-                }
-            }
-
+        }
 
 
     }
 
     public void useItem(Character character) {
         //print inventory
-        view.printinventory(model.get("0"));
+        view.printinventory(character.getInventoryController().getItemInventory());
         Scanner reader = new Scanner(System.in);
         String input = reader.nextLine();
         //find item
-        Item currentItem = model.get("0").get(Integer.parseInt(input)-1);
+        Item currentItem = character.getInventoryController().getItemInventory().get(Integer.parseInt(input) - 1);
         //check to see if consumable
         if (currentItem.get_itemType().equalsIgnoreCase("Consumable")) {
             //alter health
@@ -105,43 +103,42 @@ public class ItemController implements Serializable {
             //alter character damage output
             character.setDamage(character.getDamage() + currentItem.get_damageValue());
             // delete item from local inventory and character inventory
-            model.get("0").remove(Integer.parseInt(input)-1);
+            character.getInventoryController().getItemInventory().remove(Integer.parseInt(input) - 1);
             //character.getPlayerItemInventory().remove(Integer.parseInt(input)-1);
-        }
-        else {
-             view.notEquipable(currentItem);
+        } else {
+            view.notEquipable(currentItem);
         }
     }
 
 
     //if calling function, required inputs are roomnumber and character.
-    public void pickupItem(String roomNum, Character character){
+    public void pickupItem(String roomNum, Character character) {
         Item currentItem = model.get(roomNum).get(0);
         view.itemPromt(currentItem);
         Scanner reader = new Scanner(System.in);
         String input = reader.nextLine();
-        if (input.equalsIgnoreCase("yes")){
+        if (input.equalsIgnoreCase("yes")) {
             view.printItemDescription(currentItem);
             List<Item> playerInventory = model.get("0");
             currentItem = model.remove(roomNum).get(0);
             playerInventory.add(currentItem);
             model.put("0", playerInventory);
             //character.getPlayerItemInventory().add(currentItem);
-        }
-        else {
+        } else {
             view.itemIgnored(currentItem);
         }
     }
 
-    public void addToInventory(String roomNum, Character character){
+    public void addToInventory(String roomNum, Character character) {
         List<Item> playerInventory = model.get("0");
         Item currentItem = model.remove(roomNum).get(0);
         playerInventory.add(currentItem);
         model.put("0", playerInventory);
         //character.getPlayerItemInventory().add(currentItem);
     }
+
     //main controller calls upon this to check for any items in the room
-    public void checkRoomItem(String roomNum, Character character){
+    public void checkRoomItem(String roomNum, Character character) {
         if (model.get(roomNum).size() > 0) {
             for (int i = 0; i < model.get(roomNum).size(); i++) {
                 pickupItem(roomNum, character);
--- src/Item/ItemView.java
+++ src/Item/ItemView.java
@@ -4,41 +4,44 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- *  Author: Jayson Dasher
+ * Author: Jayson Dasher
  */
 public class ItemView implements Serializable {
 
-    public ItemView() { }
+    public ItemView() {
+    }
 
-    public void printItemName(Item item){
+    public void printItemName(Item item) {
         System.out.println(item.get_itemName());
     }
 
-    public void printItemDescription(Item item){
+    public void printItemDescription(Item item) {
         System.out.println(item.get_itemDescription());
     }
 
-    public void printNumName(Item item){
-       System.out.println(item.get_itemNumber() + ". " + item.get_itemName());
+    public void printNumName(Item item) {
+        System.out.println(item.get_itemNumbe() + ". " + item.get_itemName());
     }
 
-    public void itemPromt(Item item){
+    public void itemPromt(Item item) {
         System.out.println(item.get_itemName() + " is in your vicinity. Would you like to pick it up?");
     }
-    public void itemIgnored(Item item){
+
+    public void itemIgnored(Item item) {
         System.out.println("Item has been left on the ground.");
     }
 
-    public void printinventory(List<Item> items){
+    public void printinventory(List<Item> items) {
         for (int i = 0; i < items.size(); i++) {
             System.out.println(i + 1 + ". " + items.get(i).get_itemName());
         }
     }
-    public void notEquipable(Item item){
-       System.out.println(item.get_itemName() + " is not equipable.");
+
+    public void notEquipable(Item item) {
+        System.out.println(item.get_itemName() + " is not equipable.");
     }
 
-    public void noneEquipped(){
-       System.out.println("You do not have any items to unequip.");
+    public void noneEquipped() {
+        System.out.println("You do not have any items to unequip.");
     }
 }
